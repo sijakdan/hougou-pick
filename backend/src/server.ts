2cd50a3e8882f170287a048d49c2d8d776e00fa5
정석호 <creaticoding@gmail.com>
2025-10-11 22:00:37 +0900

    fix: prefix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,7 @@ import { healthHandler } from "./handlers/health";
 const fastify = Fastify({ logger: false });
 const { PORT = "", APIPREFIX = "" } = process.env;
 const port = PORT === "" ? 3000 : parseInt(PORT);
-const apiPrefix = APIPREFIX === "" ? "/hougou-pick" : APIPREFIX;
+const apiPrefix = APIPREFIX === "" ? "/huoguo-pick" : APIPREFIX;
 
 async function main() {
   fastify.register(
