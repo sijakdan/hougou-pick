acaf35c448558294d00fedd68ff8b576123e509c
정석호 <creaticoding@gmail.com>
2025-10-13 17:30:24 +0900

    feat: Dockerfile and hello-world

---
+++ frontend/src/app/hello-world/page.tsx
@@ -0,0 +1,13 @@
+export default function HelloWorld() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-2xl font-bold">Hello World</div>
+    <div className="w-full h-[10px]" />
+      <div className="text-lg">
+          <span className="font-bold">
+              여기는 훠궈픽의 헬로월드 페이지입니다.
+          </span>
+      </div>
+    </div>
+  );
+}
