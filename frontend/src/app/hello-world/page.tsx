3009aaa383841f7da01e47dcf438d48c713deff7
정석호 <creaticoding@gmail.com>
2025-10-22 19:44:28 +0900

    feat: deploy test

--- frontend/src/app/hello-world/page.tsx
+++ frontend/src/app/hello-world/page.tsx
@@ -5,7 +5,7 @@ export default function HelloWorld() {
     <div className="w-full h-[10px]" />
       <div className="text-lg">
           <span className="font-bold">
-              여기는 훠궈픽의 헬로월드 페이지입니다.
+              여기는 훠궈픽의 헬로월드 페이지입니다. 산들이는 귀엽습니다.
           </span>
       </div>
     </div>
